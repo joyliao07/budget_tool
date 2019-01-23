45dc15cf4ab08bf1a17d3bf067e2939d5e9942bf
Joyce Liao <joyliao07@gmail.com>
2019-01-23 15:35:23 -0800

    api working

--- budget_tool/views.py
+++ budget_tool/views.py
@@ -48,16 +48,16 @@ class TransactionCreateView(LoginRequiredMixin, CreateView):
     model = Transaction
     form_class = TransactionForm
     success_url = reverse_lazy('budget_view')
-    login_url = reverse_lazy('login')
+    login_url = reverse_lazy('auth_login')
+
+    def get_form_kwargs(self):
+        """
+        """
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
 
     def form_valid(self, form):
         """Validate form data."""
         form.instance.user = self.request.user
         return super().form_valid(form)
-
-    # def call_transactionForm(self):
-    #     current_user = self.request.user.username
-    #     form_class = TransactionForm(current_user)
-    #     return form_class
-
-
