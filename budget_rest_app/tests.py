7deedc890b19a63a0c85898440d2586c8d1d98c7
Joyce Liao <joyliao07@gmail.com>
2019-01-24 20:30:30 -0800

    testing

--- budget_rest_app/tests.py
+++ budget_rest_app/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
+from .factories import UserFactory, BudgetFactory, TransactionFactory
 
-# Create your tests here.
+
+class TestUserAPI(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('secret')
+        self.user.save()
+        self.budget = BudgetFactory()
+        self.budget.save()
+        self.c = Client()
+
+    def test_user_registration(self):
+        user = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw',
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+        }
+        response = self.client.post('/api/v1/register', user)
+        self.assertIn(b'"username":"test_user"', response.content)
+
+    def test_user_login(self):
+        import json
+        user = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw',
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+        }
+        self.client.post('/api/v1/register', user)
+        response = self.client.post('/api/v1/login', user)
+        token = json.loads(response.content)
+
+        self.assertEqual(len(token['token']), 40)
+
+    def test_user_registration_status_code(self):
+        user = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw',
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+        }
+        response = self.client.post('/api/v1/register', user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_user_login_status_code(self):
+        user = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw',
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+        }
+        self.client.post('/api/v1/register', user)
+        response = self.client.post('/api/v1/login', user)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_api_view(self):
+        user_test = {
+            'username': 'test_user',
+            'email': 'user@user.com',
+            'password': 'test_pw',
+            'first_name': 'test_first_name',
+            'last_name': 'test_last_name',
+        }
+        self.client.post('/api/v1/register', user_test)
+        response = self.client.get('/api/v1/user/3')
+        self.assertIn(b'username', response.content)
+
+    # def test_budget_list_view(self):
+    #     # To Add a new budget and see if it is in budget_list:
+    #     self.c.login(
+    #         username=self.user.username,
+    #         password='secret'
+    #     )
+    #     form_data = {
+    #         'user': 'test user',
+    #         'name': 'Budget Two',
+    #         'total_budget': 100.0,
+    #         'remaining_budget': 1.0,
+    #         'description': 'long description...'
+    #     }
+    #     self.c.post('/budget_tool/budget/add', form_data, follow=True)
+    #     res = self.c.post('/budget_tool/budget/add', form_data, follow=True)
+    #     res2 = self.client.get('api/v1/budget/', form_data, follow=True)
+    #     import pdb; pdb.set_trace()
+    #     self.assertIn(b'Budget Two', res.content)
---
+++ budget_tool/factories.py
@@ -0,0 +1,41 @@
+import factory
+from django.contrib.auth.models import User
+from budget_tool.models import Budget, Transaction
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Create a test user for writing tests."""
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+
+
+class BudgetFactory(factory.django.DjangoModelFactory):
+    """Create a test budget for writing tests."""
+
+    class Meta:
+        model = Budget
+
+    user = factory.SubFactory(UserFactory)
+    name = factory.Faker('word')
+    total_budget = 100.0
+    remaining_budget = 100.0
+
+
+class TransactionFactory(factory.django.DjangoModelFactory):
+    """Create a test Transaction for writing tests."""
+
+    class Meta:
+        model = Transaction
+
+    assigned_to = factory.SubFactory(UserFactory)
+    budget = factory.SubFactory(BudgetFactory)
+    transaction_type = 'deposit'
+    amount = 15.0
+    description = factory.Faker('paragraph')
+
