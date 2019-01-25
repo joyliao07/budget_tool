56f9d2aa67f38d083421b01086546595d526c2c1
Joyce Liao <joyliao07@gmail.com>
2019-01-24 16:35:58 -0800

    http GET :8000/api/v1/transaction/ view issue

--- budget_rest_app/urls.py
+++ budget_rest_app/urls.py
@@ -1,10 +1,18 @@
-from .views import RegisterApiView, UserApiView
 from django.urls import path
 from rest_framework.authtoken import views
+from .views import RegisterApiView, UserApiView, BudgetListApiView, BudgetDetailApiView, TransactionListApiView, TransactionDetailApiView
 
 
 urlpatterns = [
     path('user/<int:pk>', UserApiView.as_view(), name='user-detail'),
     path('register', RegisterApiView.as_view(), name='register'),
     path('login', views.obtain_auth_token),
+    # path('budget/', BudgetListApiView.as_view(), name='budget-list-api'),
+    path('budget/', BudgetListApiView.as_view(), name='budget-list-api'),
+    # path('budget/<int:pk>', BudgetDetailApiView.as_view(), name='budget-detail-api'),
+    path('budget/<int:pk>', BudgetDetailApiView.as_view(), name='budget-detail-api'),
+    # path('transaction/', TransactionListApiView.as_view(), name='transaction-list-api'),
+    path('transaction/', TransactionListApiView.as_view(), name='transaction-list-api'),
+    # path('transaction/<int:pk>', TransactionDetailApiView.as_view(), name='transaction-detail-api'),
+    path('transaction/<int:pk>', TransactionDetailApiView.as_view(), name='transaction-detail-api'),
 ]
--- budget_rest_app/views.py
+++ budget_rest_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from .serializers import UserSerializer, User
+from .serializers import UserSerializer, User, BudgetSerializer, Budget, TransactionSerializer, Transaction
 
 
 class RegisterApiView(generics.CreateAPIView):
@@ -15,3 +16,46 @@ class UserApiView(generics.RetrieveAPIView):
 
     def get_queryset(self):
         return User.objects.filter(id=self.kwargs['pk'])
+
+
+class BudgetListApiView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = BudgetSerializer
+
+    def get_queryset(self):
+        return Budget.objects.filter(
+            user__username=self.request.user.username)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id)
+
+
+class BudgetDetailApiView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = BudgetSerializer
+
+    def get_queryset(self):
+        return Budget.objects.filter(
+            user__username=self.request.user.username)
+
+
+class TransactionListApiView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        return Transaction.objects.filter(
+            budget__user__username=self.request.user.username)
+
+
+class TransactionDetailApiView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        return Transaction.objects.filter(
+            budget__user__username=self.request.user.username)
